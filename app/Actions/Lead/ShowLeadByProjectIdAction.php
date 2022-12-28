0a3342942f4d95a1a62690d9809838bc07f80e1f
Atef Rihane <rihaneatef@gmail.com>
2022-12-28 17:51:58 +0100

    lead join fixes

--- app/Actions/Lead/ShowLeadByProjectIdAction.php
+++ app/Actions/Lead/ShowLeadByProjectIdAction.php
@@ -16,9 +16,25 @@ class ShowLeadByProjectIdAction
         // search Member owner by userId
         return Lead::query()
             ->with(['user', 'project'])
-            ->where(['project_id' => $projectId])
-            ->where('user_id', auth()->id())
-            ->orWhereHas('users', fn ($q) => $q->where('users.id', auth()->id()))
+            ->where(function ($q) use ($projectId) {
+                $q->where(['project_id' => $projectId])
+                    ->where('user_id', auth()->id())
+                    ->OrwhereExists(function ($q) use ($projectId) {
+                        $q->select("lead_user.lead_id")
+                            ->from('lead_user')
+                            ->where('lead_user.user_id', auth()->id())
+                            ->where('lead_id', function ($query) use ($projectId) {
+                                $query->select('id')
+                                    ->from('leads')
+                                    ->where('project_id', $projectId)
+                                    ->whereRaw('lead_user.lead_id = leads.id')
+                                    ->limit(1);
+                            })
+                            ->whereRaw('lead_user.lead_id = leads.id');
+                    });
+                // ->orWhereHas('users', fn ($q) => $q->where('users.lead_id', auth()->id()));
+            })
+
             ->get();
     }
 }
